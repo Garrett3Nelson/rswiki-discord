101b6438ba719f9137cdb9e68b631c54afa95fc9
Garrett3Nelson <garrett3nelson@gmail.com>
2022-12-05 16:50:42 -0400

    Added function to allow `latest` to accept any combination of names/itemIDs as arguments. Improved documentation

--- wikibot.py
+++ wikibot.py
@@ -26,12 +26,24 @@ user_agent = os.getenv('USER_AGENT')
 
 
 def name_conversion(name):
+    """
+    Given a string `name`, return the `id` of the first match from RSWiki Mapping API.
+
+    :param name: The string to search for in the `json` object.
+    :return: The `id` of the matching element, or the string 'No match' if no matching element was found.
+    """
+    # Create a Mapping object with the specified user_agent parameter.
     temp_map = Mapping(user_agent=user_agent)
+
+    # Use a list comprehension to extract the id of each element where the name field matches the input name,
+    # ignoring the case of the strings.
     response = [x['id'] for x in temp_map.json if name.lower() == x['name'].lower()]
 
+    # If the response list is empty, then no matching elements were found and return the string 'No match'.
     if len(response) == 0:
         return 'No match'
 
+    # Otherwise, return the first element of the response list, which is the id of the matching element.
     return response[0]
 
 
@@ -50,20 +62,23 @@ async def hello(ctx):
 @option('command', description='Which command to provide help (or all)', required=False, default='all')
 async def help(ctx: discord.ApplicationContext, command: str):
     valid_commands = {'all': """Here is a list of valid commands:
-`/latest`: Returns the latest real-time price & volume for a given item
+`/latest`: Returns the latest real-time price & volume for given items
 `/average`: Returns the average real-time price & volume over a specified time period for a given item
 `/timeseries`: Returns a timeseries graph of the latest 365 price & volume data for a specific time step for a given item
 `/itemid`: Look up an item by name to find out the item ID""",
-                      'latest': """Returns the latest real-time price & volume for a given item
-Required Parameters: None (defaults to `all`)
-Optional Parameters: `ids` - Provide the itemID(s) to provide the latest price & volume for. If looking up multiple items, separate with |""",
-                      'average': """Returns the average real-time price & volume over a specified time period for a given item
+                      'latest': """`latest`
+Returns the latest real-time price & volume for given item(s)
+Required Parameters: `items` - Provide the item ID(s) or name(s) to provide the latest price & volume for. If looking up multiple items, separate with |""",
+                      'average': """`average`
+Returns the average real-time price & volume over a specified time period for a given item
 Required Parameters: `timestep` - The time period to request an average. RSWiki accepts 5m and 1h as valid arguments
 `id` OR `name` - The item name or ID. If you provide both, item name will be ignored. Name is case insensitive.""",
-                      'timeseries': """Returns a timeseries graph of the last 365 points of price & volume data for a specific time step for a given item
+                      'timeseries': """`timeseries`
+Returns a timeseries graph of the last 365 points of price & volume data for a specific time step for a given item
 Required Parameters: `timestep` - The time step to average data. RSWiki accepts 5m and 1h as valid arguments
 `id` OR `name` - The item name or ID. If you provide both, item name will be ignored.Name is case insensitive.""",
-                      'itemid': """Look up an item by name to find out the item ID
+                      'itemid': """`itemid`
+Look up an item by name to find out the item ID
 Required Parameters: `name` - The item name to look up. Name is case insensitive and can be partial.
 'coal' will return 'coal' and 'charcoal' results."""}
 
@@ -73,15 +88,55 @@ Required Parameters: `name` - The item name to look up. Name is case insensitive
         await ctx.respond('Your command is not valid, use `/help` with no command to see valid documented commands')
 
 
-@bot.slash_command(description='Get latest real-time prices')
-@option('ids', description='Specific IDs (separate with |) or blank for all', required=False, default='')
-async def latest(ctx: discord.ApplicationContext,
-                 ids: str):
+def convert_names_to_ids(id_string: str):
+    """
+    Converts any non-numeric elements in the input string to numeric elements using the name_conversion function, and
+    then returns a new string with the numeric elements.
 
-    if len(ids) == 0:
-        real_time = Latest(user_agent=user_agent)
-    else:
-        real_time = Latest(id=ids, user_agent=user_agent)
+    Args:
+        id_string (str): A string formatted '1|2|3' or 'Test|Text|Name'
+
+    Returns:
+        str: A new string with the same elements as the input string, but with any non-numeric elements converted to
+             numeric elements using the name_conversion function. Any elements which fail name_conversion are removed
+    """
+
+    # Split the input string into a list of elements.
+    id_list = id_string.split('|')
+
+    # Iterate over the elements in the list.
+    for i, item_id in enumerate(id_list):
+        # If the element is numeric, skip it.
+        if item_id.isnumeric():
+            continue
+
+        # If the element is not numeric, it is a name.
+        # Convert the name to a numeric element using the name_conversion function.
+        temp_id = name_conversion(item_id)
+
+        # If the converted element is numeric, replace the original non-numeric element with the numeric element
+        # in the id_list array.
+        if isinstance(temp_id, int):
+            id_list[i] = str(temp_id)
+        else:
+            id_list.pop(i)
+
+    # Return a string with the same elements as the input string, but with any non-numeric elements converted to
+    # numeric elements using the name_conversion function.
+    return '|'.join(id_list)
+
+
+@bot.slash_command(description='Get latest real-time prices')
+@option('items', description='Specific item IDs or names(separate with | for multiple)', required=True, default='')
+async def latest(ctx: discord.ApplicationContext,
+                 items: str):
+
+    ids = convert_names_to_ids(items)
+
+    # if len(ids) == 0:
+    #    real_time = Latest(user_agent=user_agent)
+    # else:
+    real_time = Latest(id=ids, user_agent=user_agent)
 
     response = json.dumps(real_time.content, indent=4)
 
